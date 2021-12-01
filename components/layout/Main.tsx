ba2d09e100b14644229c9fb4a3d9e53c69f69a31
lanmai14 <lan.mai@zodinet.tech>
2021-12-01 16:20:09 +0700

    update status

--- components/layout/Main.tsx
+++ components/layout/Main.tsx
@@ -2,8 +2,7 @@ import { LayoutProps } from '@/models/index';
 import React, { useLayoutEffect } from 'react';
 import { Layout, Menu } from 'antd';
 import { LaptopOutlined, NotificationOutlined, UserOutlined } from '@ant-design/icons';
-import Link from 'next/link'
-
+import Link from 'next/link';
 const { SubMenu } = Menu;
 const { Header, Content, Sider } = Layout;
 
@@ -16,47 +15,49 @@ export function MainLayout({ children }: LayoutProps) {
 
   return (
     <Layout>
-      <Header className='header'>
-        <div className='logo' />
-        <Menu theme='dark' mode='horizontal' defaultSelectedKeys={['2']}>
+      <Header className="header">
+        <div className="logo" />
+        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
           <Link href="/">
-            <a><Menu.Item key='1'>nav 1</Menu.Item></a>
+            <a>
+              <Menu.Item key="1">nav 1</Menu.Item>
+            </a>
           </Link>
-          <Menu.Item key='2'>nav 2</Menu.Item>
-          <Menu.Item key='3'>nav 3</Menu.Item>
+          <Menu.Item key="2">nav 2</Menu.Item>
+          <Menu.Item key="3">nav 3</Menu.Item>
         </Menu>
       </Header>
       <Layout>
-        <Sider width={200} className='site-layout-background'>
+        <Sider width={200} className="site-layout-background">
           <Menu
-            mode='inline'
+            mode="inline"
             defaultSelectedKeys={['1']}
             defaultOpenKeys={['sub1']}
             style={{ height: '100%', borderRight: 0 }}
           >
-            <SubMenu key='sub1' icon={<UserOutlined />} title='subnav 1'>
-              <Menu.Item key='1'>option1</Menu.Item>
-              <Menu.Item key='2'>option2</Menu.Item>
-              <Menu.Item key='3'>option3</Menu.Item>
-              <Menu.Item key='4'>option4</Menu.Item>
+            <SubMenu key="sub1" icon={<UserOutlined />} title="subnav 1">
+              <Menu.Item key="1">option1</Menu.Item>
+              <Menu.Item key="2">option2</Menu.Item>
+              <Menu.Item key="3">option3</Menu.Item>
+              <Menu.Item key="4">option4</Menu.Item>
             </SubMenu>
-            <SubMenu key='sub2' icon={<LaptopOutlined />} title='subnav 2'>
-              <Menu.Item key='5'>option5</Menu.Item>
-              <Menu.Item key='6'>option6</Menu.Item>
-              <Menu.Item key='7'>option7</Menu.Item>
-              <Menu.Item key='8'>option8</Menu.Item>
+            <SubMenu key="sub2" icon={<LaptopOutlined />} title="subnav 2">
+              <Menu.Item key="5">option5</Menu.Item>
+              <Menu.Item key="6">option6</Menu.Item>
+              <Menu.Item key="7">option7</Menu.Item>
+              <Menu.Item key="8">option8</Menu.Item>
             </SubMenu>
-            <SubMenu key='sub3' icon={<NotificationOutlined />} title='subnav 3'>
-              <Menu.Item key='9'>option9</Menu.Item>
-              <Menu.Item key='10'>option10</Menu.Item>
-              <Menu.Item key='11'>option11</Menu.Item>
-              <Menu.Item key='12'>option12</Menu.Item>
+            <SubMenu key="sub3" icon={<NotificationOutlined />} title="subnav 3">
+              <Menu.Item key="9">option9</Menu.Item>
+              <Menu.Item key="10">option10</Menu.Item>
+              <Menu.Item key="11">option11</Menu.Item>
+              <Menu.Item key="12">option12</Menu.Item>
             </SubMenu>
           </Menu>
         </Sider>
         <Layout style={{ padding: '0 24px 24px' }}>
           <Content
-            className='site-layout-background'
+            className="site-layout-background"
             style={{
               padding: 24,
               margin: 0,
@@ -70,4 +71,3 @@ export function MainLayout({ children }: LayoutProps) {
     </Layout>
   );
 }
-
--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,16 +1,55 @@
-import { DatePicker, Space } from 'antd';
-import { NextPageWithLayout } from '@/models/common.interface';
+import { Form, Input, Button, Checkbox } from 'antd';
+import { NextPage } from 'next';
+const Login: NextPage = () => {
+  const onFinish = (values: any) => {
+    console.log('Success:', values);
+  };
 
-function onChange(date: any, dateString: any) {
-  console.log(date, dateString);
-}
+  const onFinishFailed = (errorInfo: any) => {
+    console.log('Failed:', errorInfo);
+  };
 
-const Login: NextPageWithLayout = () => {
   return (
-    <div className={'LoginContainer'}>
-      <Space direction="vertical">
-        <DatePicker onChange={onChange} />
-      </Space>
+    <div className={'Container'}>
+      <h1> Đăng nhập</h1>
+      <Form
+        className="form"
+        name="basic"
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 16 }}
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
+      >
+        <Form.Item
+          className="form__email"
+          label="Email"
+          name="email"
+          rules={[{ required: true, message: 'Please input your email!' }]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          className="form__email"
+          label="Password"
+          name="password"
+          rules={[{ required: true, message: 'Please input your password!' }]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item name="remember" valuePropName="checked" wrapperCol={{ offset: 8, span: 16 }}>
+          <Checkbox>Remember me</Checkbox>
+        </Form.Item>
+
+        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+          <Button type="primary" htmlType="submit">
+            Đăng nhập
+          </Button>
+        </Form.Item>
+      </Form>
     </div>
   );
 };
