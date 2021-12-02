91a6c1ad1acb20cadc8ac29651369192f3367592
lanmai14 <lan.mai@zodinet.tech>
2021-12-02 14:42:46 +0700

    add login page

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,5 +1,7 @@
+import FacebookOutlined from '@ant-design/icons/lib/icons/FacebookOutlined';
 import { Form, Input, Button, Checkbox } from 'antd';
 import { NextPage } from 'next';
+import React from 'react';
 const Login: NextPage = () => {
   const onFinish = (values: any) => {
     console.log('Success:', values);
@@ -11,45 +13,42 @@ const Login: NextPage = () => {
 
   return (
     <div className={'Container'}>
-      <h1> Đăng nhập</h1>
-      <Form
-        className="form"
-        name="basic"
-        labelCol={{ span: 8 }}
-        wrapperCol={{ span: 16 }}
-        initialValues={{ remember: true }}
-        onFinish={onFinish}
-        onFinishFailed={onFinishFailed}
-        autoComplete="off"
-      >
-        <Form.Item
-          className="form__email"
-          label="Email"
-          name="email"
-          rules={[{ required: true, message: 'Please input your email!' }]}
+      <div className="Container__login">
+        <h1> Đăng nhập</h1>
+        <Form
+          className="form"
+          name="basic"
+          labelCol={{ span: 8 }}
+          wrapperCol={{ span: 16 }}
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          autoComplete="off"
         >
-          <Input />
-        </Form.Item>
+          <Input placeholder="Email" className="form__email" />
 
-        <Form.Item
-          className="form__email"
-          label="Password"
-          name="password"
-          rules={[{ required: true, message: 'Please input your password!' }]}
-        >
-          <Input.Password />
-        </Form.Item>
+          <Input placeholder="Password" className="form__password" />
 
-        <Form.Item name="remember" valuePropName="checked" wrapperCol={{ offset: 8, span: 16 }}>
-          <Checkbox>Remember me</Checkbox>
-        </Form.Item>
+          <Form.Item valuePropName="checked" className="form__box">
+            <Checkbox>Remember me</Checkbox>
+          </Form.Item>
 
-        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-          <Button type="primary" htmlType="submit">
+          <Button type="primary" className="button__login">
             Đăng nhập
           </Button>
-        </Form.Item>
-      </Form>
+          <Button type="primary" className="button__register">
+            Đăng ký
+          </Button>
+
+          <Button className="form__facebook">
+            <FacebookOutlined />
+            <a href="https://www.facebook.com/">Sign in with Facebook</a>
+          </Button>
+        </Form>
+      </div>
+      <div className="Container__adv">
+        <p>Ticket BOX</p>
+      </div>
     </div>
   );
 };
--- pages/register/index.tsx
+++ pages/register/index.tsx
@@ -1,6 +1,59 @@
+import { Form, Input, Button, Checkbox } from 'antd';
+
 import { NextPage } from 'next';
-const Login: NextPage = () => {
-  return <div className={'Container'}></div>;
+const Register: NextPage = () => {
+  const onFinish = (values: any) => {
+    console.log('Success:', values);
+  };
+
+  const onFinishFailed = (errorInfo: any) => {
+    console.log('Failed:', errorInfo);
+  };
+
+  return (
+    <div className="Container">
+      <h1>Đăng ký</h1>
+      <Form
+        name="basic"
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 16 }}
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
+      >
+        <Form.Item
+          label="Username"
+          name="username"
+          rules={[{ required: true, message: 'Please input your username!' }]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[{ required: true, message: 'Please input your password!' }]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item
+          label="RePassword"
+          name="password"
+          rules={[{ required: true, message: 'Please input your password!' }]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  );
 };
 
-export default Login;
+export default Register;
