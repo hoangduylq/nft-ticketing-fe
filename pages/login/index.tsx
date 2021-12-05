03d3eac937b5aa4a214350e211a29c732a3d1043
lanmai14 <lan.mai@zodinet.tech>
2021-12-05 13:07:22 +0700

    changes

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -12,42 +12,45 @@ const Login: NextPage = () => {
   };
 
   return (
-    <div className={'Container'}>
-      <div className="Container__login">
-        <h1> Đăng nhập</h1>
-        <Form
-          className="form"
-          name="basic"
-          labelCol={{ span: 8 }}
-          wrapperCol={{ span: 16 }}
-          initialValues={{ remember: true }}
-          onFinish={onFinish}
-          onFinishFailed={onFinishFailed}
-          autoComplete="off"
-        >
-          <Input placeholder="Email" className="form__email" />
+    <div id="Main">
+      <div id="Main__header"></div>
+      <div id="Main__content">
+        <div id="Main__content__login">
+          <h1> Đăng nhập</h1>
+          <Form
+            id="form"
+            name="basic"
+            labelCol={{ span: 8 }}
+            wrapperCol={{ span: 16 }}
+            initialValues={{ remember: true }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            autoComplete="off"
+          >
+            <Input placeholder="Email" id="form__email" />
 
-          <Input placeholder="Password" className="form__password" />
+            <Input placeholder="Password" id="form__password" />
 
-          <Form.Item valuePropName="checked" className="form__box">
-            <Checkbox>Remember me</Checkbox>
-          </Form.Item>
+            <Form.Item valuePropName="checked" id="form__box">
+              <Checkbox>Remember me</Checkbox>
+            </Form.Item>
 
-          <Button type="primary" className="button__login">
-            Đăng nhập
-          </Button>
-          <Button type="primary" className="button__register">
-            Đăng ký
-          </Button>
+            <Button type="primary" id="button__login">
+              Đăng nhập
+            </Button>
+            <Button type="primary" id="button__register">
+              Đăng ký
+            </Button>
 
-          <Button className="form__facebook">
-            <FacebookOutlined />
-            <a href="https://www.facebook.com/">Sign in with Facebook</a>
-          </Button>
-        </Form>
-      </div>
-      <div className="Container__adv">
-        <p>Ticket BOX</p>
+            <Button id="form__facebook">
+              <FacebookOutlined className={undefined} />
+              <a href="https://www.facebook.com/">Sign in with Facebook</a>
+            </Button>
+          </Form>
+        </div>
+        <div id="Main__content__adv">
+          <p>Ticket BOX</p>
+        </div>
       </div>
     </div>
   );
