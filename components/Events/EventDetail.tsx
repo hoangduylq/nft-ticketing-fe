c8a4a719b271afe87a9625df302c09c5826d430d
ngando2028 <ngan.do@zodinet.tech>
2021-12-10 17:40:25 +0700

    upload form detail

--- components/Events/EventDetail.tsx
+++ components/Events/EventDetail.tsx
@@ -1,13 +1,12 @@
-import { Form, Input, Select, Row, Col } from 'antd';
+import { Form, Input, Row, Col, DatePicker } from 'antd';
 import { FormInstance } from 'antd/es/form';
 import React from 'react';
 
 const EventDetail: React.FC = () => {
-  const { Option } = Select;
   const formRef = React.createRef<FormInstance>();
 
   const layout = {
-    labelCol: { span: 4 },
+    labelCol: { span: 6 },
     wrapperCol: { span: 12 },
   };
 
@@ -22,46 +21,90 @@ const EventDetail: React.FC = () => {
           <Col span={24}>
             <h1 className="event-detail__heading">Event Information</h1>
           </Col>
-          <Col span={14}>
-            <Form.Item name="name" label="Event Name" rules={[{ required: true }]}>
+          <Col span={12} offset={2}>
+            <Form.Item name="name" label="Event name" rules={[{ required: true }]}>
               <Input />
             </Form.Item>
-            <Form.Item name="gender" label="Gender" rules={[{ required: true }]}>
-              <Select
-                placeholder="Select a option and change input text above"
-                // onChange={onGenderChange}
-                allowClear
-              >
-                <Option value="male">male</Option>
-                <Option value="female">female</Option>
-                <Option value="other">other</Option>
-              </Select>
+            <Form.Item name="eventPlaceName" label="Event place name" rules={[{ required: true }]}>
+              <Input />
             </Form.Item>
             <Form.Item
-              noStyle
-              shouldUpdate={(prevValues, currentValues) =>
-                prevValues.gender !== currentValues.gender
-              }
+              name="eventPlaceAddress"
+              label="Event place address"
+              rules={[{ required: true }]}
             >
-              {({ getFieldValue }) =>
-                getFieldValue('gender') === 'other' ? (
-                  <Form.Item
-                    name="customizeGender"
-                    label="Customize Gender"
-                    rules={[{ required: true }]}
-                  >
-                    <Input />
-                  </Form.Item>
-                ) : null
-              }
+              <Input />
             </Form.Item>
-            {/* <Form.Item>
-              <Button type="primary" htmlType="submit">
-                Submit
-              </Button>
+
+            <Form.Item name="ticketPrice!" label="Ticket Price" rules={[{ required: true }]}>
+              <Input />
+            </Form.Item>
+            <Form.Item name="maxTicketOrder" label="Max ticket order" rules={[{ required: true }]}>
+              <Input />
+            </Form.Item>
+            <Form.Item name="minTicketOrder" label="Min ticket order" rules={[{ required: true }]}>
+              <Input />
+            </Form.Item>
+            <Form.Item
+              name="organizationInfo"
+              label="Organization Info"
+              rules={[{ required: true }]}
+            >
+              <Input />
+            </Form.Item>
+            <Form.Item
+              name="organizationEmail"
+              label="Organization Email"
+              rules={[{ required: true }]}
+            >
+              <Input />
+            </Form.Item>
+            <Form.Item
+              name="organizationPhone"
+              label="Organization Phone"
+              rules={[{ required: true }]}
+            >
+              <Input />
+            </Form.Item>
+            <Form.Item
+              name="organizationAddress"
+              label="Organization Address"
+              rules={[{ required: true }]}
+            >
+              <Input />
+            </Form.Item>
+          </Col>
+          <Col span={10}>
+            <Form.Item name="saleStartDate" label="Sale Start Date" rules={[{ required: true }]}>
+              <DatePicker />
+            </Form.Item>
+            <Form.Item name="saleEndDate" label="Sale End Date" rules={[{ required: true }]}>
+              <DatePicker />
+            </Form.Item>
+            <Form.Item name="eventStartDate" label="Event Start Date" rules={[{ required: true }]}>
+              <DatePicker />
+            </Form.Item>
+            <Form.Item name="eventEndDate" label="Event End Date" rules={[{ required: true }]}>
+              <DatePicker />
+            </Form.Item>
+            {/* <Form.Item name="logoUrl" label="Logo Event">
+              <Upload
+                name="avatar"
+                listType="picture-card"
+                className="avatar-uploader"
+                showUploadList={false}
+                action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+                beforeUpload={beforeUpload}
+                onChange={this.handleChange}
+              >
+                {imageUrl ? (
+                  <img src={imageUrl} alt="avatar" style={{ width: '100%' }} />
+                ) : (
+                  uploadButton
+                )}
+              </Upload>
             </Form.Item> */}
           </Col>
-          <Col span={10}></Col>
         </Row>
       </Form>
     </main>
